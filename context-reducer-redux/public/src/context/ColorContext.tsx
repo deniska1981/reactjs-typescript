1d93550f04eadb1fb20f0e8c98115793bffbd830
deniska1981 <dtkalec81@gmail.com>
2023-03-09 20:18:38 +0100

    1

--- context-reducer-redux/public/src/context/ColorContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-type ColorContextType = number;
-
-export const ColorContext = createContext<ColorContextType>(10);
--- context-reducer-redux/public/src/context/ThemeContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-type ThemeContextType = number;
-
-export const ThemeContext = createContext<ThemeContextType>(225);
